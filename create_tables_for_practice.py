83d21d8ceb2cd16f20be4430392f48e52c98d6b0
BGrummler <127844288+BGrummler@users.noreply.github.com>
2023-07-06 17:06:21 +0200

    creates a database and 2 tables
    for practice purpose

---
+++ create_tables_for_practice.py
@@ -0,0 +1,69 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def main():
+    database = r"C:\FIAE\Python\test.db"
+
+    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS User (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        Password text
+                                    ); """
+
+    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS Chat (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL,
+                                    priority integer,
+                                    User_id integer NOT NULL,
+                                    Group_id integer NOT NULL,
+                                    begin_date text NOT NULL,
+                                    end_date text NOT NULL,
+                                    FOREIGN KEY (User_id) REFERENCES User (id)
+                                );"""
+
+    # create a database connection
+    conn = create_connection(database)
+
+    # create tables
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_projects_table)
+
+        # create tasks table
+        create_table(conn, sql_create_tasks_table)
+    else:
+        print("Error! cannot create the database connection.")
+
+
+if __name__ == '__main__':
+    main()
