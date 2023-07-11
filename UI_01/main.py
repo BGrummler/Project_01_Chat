feaa59df464fc0d8124439be4fb32d22781ad37d
Madhatter <kraehenkind@googlemail.com>
2023-07-11 15:18:05 +0200

    Login Screen

--- UI_01/main.py
+++ UI_01/main.py
@@ -1,29 +1,36 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-class Widgets(BoxLayout):
+class Manager(ScreenManager):
+    pass
+
+class LogInScreen(Screen):
+   
+    def validate_login(self, widget):
+        print("aufgerufen")
+
+
+class ChatScreen(Screen):
     pass
 
 
 class RightColumn(BoxLayout):
 
     def testChat(self):
-        left_label = self.parent.ids.left_column.ids.ScrollChat.ids.left_label
-        chat_input = self.parent.ids.left_column.ids.chat_input
+        left_label = self.parent.parent.ids.left_column.ids.ScrollChat.ids.left_label
+        chat_input = self.parent.parent.ids.left_column.ids.chat_input
 
         if chat_input.text != "":
             left_label.text =  left_label.text + "\n" + chat_input.text
             chat_input.text = ""
-            chat_input.focus = True
-
+            
 
 class LeftColumn(BoxLayout):
     pass
 
-
 class ChatProject(App):
     pass
-
 
 ChatProject().run()
