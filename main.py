be9a6d10c67d20a5763f7d08c80768526b960831
Madhatter <kraehenkind@googlemail.com>
2023-07-12 11:18:52 +0200

    LoginScreen updated
    LoginScreen checks db for user/password and if verified ChatScreen  is called.

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+import login_001 as lg
 
 
 class Manager(ScreenManager):
     pass
 
 class LogInScreen(Screen):
-        
+
+    login_counter = 0 #to be used later for finite attempts
+
     def validate_login(self, widget):
-        print("ja")
-            
+        user_name = self.ids.user_text.ids.username.text
+        user_pw = self.ids.user_pw.ids.passwort.text
+        if user_name == "" or user_pw == "":
+            pass
+        elif lg.user_login(user_name, user_pw):
+            app = App.get_running_app()
+            app.root.current = "ChatScreen"
+        else:
+            self.ids.user_text.ids.username.text = ""
+            self.ids.user_pw.ids.passwort.text = ""
 
 class ChatScreen(Screen):
     pass
-
 
 class RightColumn(BoxLayout):
 
@@ -27,7 +36,6 @@ class RightColumn(BoxLayout):
             left_label.text =  left_label.text + "\n" + chat_input.text
             chat_input.text = ""
             
-
 class LeftColumn(BoxLayout):
     pass
 
