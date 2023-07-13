e0bdac183563649a4191b245ff96a8a91cb46d56
Madhatter <kraehenkind@googlemail.com>
2023-07-13 14:11:43 +0200

    Mino update
    - Tab textfield change enabled
    - New account button prepared

--- main.py
+++ main.py
@@ -5,26 +5,32 @@ from kivy.core.window import Window
 from kivy.metrics import dp
 import login_001 as lg
 
+current_user        = None
+current_user_pw     = None
 
 class Manager(ScreenManager):
     pass
 
 class LogInScreen(Screen):
 
+    login_counter = 0 #to be used later for finite attempts
+    
     def on_pre_enter(self):
         Window.size = (dp(350), dp(220))
 
-    login_counter = 0 #to be used later for finite attempts
-
     def validate_login(self):
+        global current_user
+        global current_user_pw     
         user_name = self.ids.user_text.ids.username.text
-        user_pw = self.ids.user_pw.ids.passwort.text
+        user_pw   = self.ids.user_pw.ids.passwort.text
         if lg.user_login(user_name, user_pw):
-            app = App.get_running_app()
+            app              = App.get_running_app()
             app.root.current = "ChatScreen"
+            current_user     = user_name
+            current_user_pw  = user_pw
         else:
             self.ids.user_text.ids.username.text = ""
-            self.ids.user_pw.ids.passwort.text = ""
+            self.ids.user_pw.ids.passwort.text   = ""
 
 class ChatScreen(Screen):
 
