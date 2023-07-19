df23421fea2f8267a3867f0e1f41fe88d6d00133
BGrummler <127844288+BGrummler@users.noreply.github.com>
2023-07-19 07:09:16 +0200

    Update Communication_P1_Chat.py
    added sql query function to list all users for invite friend, needs to be improved to only show not private and not already friended ones

--- Communication_P1_Chat.py
+++ Communication_P1_Chat.py
@@ -9,14 +9,15 @@ database_path = current_path/"test.db"
 # this list isnt used att all i keep it for database reference
 tables_p1_chat = {
     "server": {
-        "server.user": ["Nickname PK", "Message.MessageID FK", "Password", "Private_Mode", "Hidden_Mode"],
-        "server.group": ["Group_Name PK"],
+        "server.user": ["Nickname PK", "Message.MessageID FK", "Password", "pending", "Private_Mode", "Hidden_Mode"],
+        "server.group": ["Group_Name PK","admins fk"],
+        "server.permissions": ["permissions_id", "user.nickname fk" "target usergroup fk"],
         "server.usergroup": ["UserGroup_ID", "User.Nickname FK", "Group.Groupname FK"],
         "server.message": ["Message_ID PK", "Message_From FK", "Message_To FK", "Message_Body"],
     },
     "client": {
         "client.chats": ["server.group.Group_Name FK","server.user.Nickname FK" ],
-        "client.message": ["Message_ID PK FK", "Message_From FK", "Message_To FK", "Message_Body FK"]
+        "client.message": ["Message_ID PK FK", "Message_From FK", "Message_To FK", "Message_Body TK"] #Temp Key
     },
 }
 
@@ -29,8 +30,17 @@ sql_queries = {
         # Add other queries as needed
 }
 
-#decorator function to handle all try → except logic and the cursor
 def try_except(function): 
+    """
+    decorator function to handle all try → except logic and the cursor
+
+    Arguments:
+        function: function to wrap
+        c: sqlite3.cursor
+    
+    Returns:
+        wrapped function
+    """
     def wrapper(*args, **kwargs):
         global conn
         try:
@@ -41,10 +51,15 @@ def try_except(function):
     return wrapper
 
 
-def create_connection(db_file):
+def create_connection(db_file:str) -> sqlite3.Connection:
     """ 
     creates a database connection to the SQLite databas specified by db_file
-    :return: Connection object or None
+
+    Arguments:
+        db_file path to database
+
+    Returns: 
+        Connection object or None
     """
     conn = None
     try:
@@ -52,39 +67,88 @@ def create_connection(db_file):
         return conn
     except Error as e:
         print(e)
-
     return conn
 
 
 @try_except
-def create_account(c, new_user, new_password):
+def create_account(c:sqlite3.Cursor, new_user:str, new_password:str):
+    """
+    create new account on the database
+
+    Arguments:
+        c: sqlite 3 cursor object
+        new_user: Nickname for the new user
+        password: password for the new user
+
+    Returns:
+        None
+    """
     insert_query = "INSERT INTO User (Nickname, Password) VALUES (?, ?)"            
     c.execute(insert_query, (new_user, new_password))
     conn.commit()
 
 
 @try_except
-def does_nickname_exist(c, nickname):   
+def does_nickname_exist(c:sqlite3.Cursor, nickname:str) -> tuple:
+    """
+    checks if the username already exists
+
+    Arguments:
+        c: cursor for database communication
+        nickname: nickname to seach for in the database
+
+    Returns:
+        Tuple with the nickname if any, else empty tuple
+    """
     select_query = "SELECT * FROM User WHERE Nickname = ?"
     c.execute(select_query, (nickname,))
     return c.fetchone()
 
-#outdated
+
+@try_except
+def select_users(c:sqlite3.Cursor) -> list[tuple]:
+    """
+    TODO
+    """
+    select_query = "SELECT * FROM User"
+    c.execute(select_query)
+    return c.fetchall()
+
+
 @try_except
 def select_table(c, table, column = '*', WHERE = ''):
+    """
+    #outdated
+    """
     select_table_sql = f"SELECT {column} FROM {table} {WHERE};"
     c.execute(select_table_sql)
     return c.fetchall()
 
-#outdated
+
 @try_except
-def filter_results_by_WHERE(table, new_user): #If needed overwrite the default WHERE = '' with WHERE = filter_results_by_WHERE in the select_table() FUNKTION 
+def filter_results_by_WHERE(table, new_user): 
+    """
+    #outdated
+
+    If needed overwrite the default WHERE = '' with WHERE = filter_results_by_WHERE in the select_table() FUNKTION 
+    """
     where_string_sql = f"WHERE {table} = '{new_user}'"
     return where_string_sql
 
 
 @try_except
-def user_login(c, nickname, password):
+def user_login(c:sqlite3.Cursor, nickname:str, password:str):
+    """
+    compare entered username and password witht he database
+
+    Arguments:
+        c: cursor for database communication
+        nickname: input from user
+        password: input from user
+
+    Returns:
+        bool: True if user exists and corresponding password is correct 
+    """
     select_query = "SELECT Nickname, Password FROM User WHERE Nickname = ?"
     c.execute(select_query, (nickname,))
     check_name_pw = c.fetchone()
